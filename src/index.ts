9933a48ff19bd130a25624b3bdc806365b685abf
ilanlavi <refaelberlin@gmail.com>
2022-10-22 23:27:11 +0200

    typescript todo list

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,70 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// Selectors ///////////////////////////////////////////
+
+// querySelector example
+const listContainer = document.querySelector<HTMLUListElement>('#list');
+// element by id example
+const todoInput = document.getElementById('new-task-title') as HTMLInputElement;
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+
+//creating tasks array
+const tasks: Task[] = loadTasks();
+tasks.forEach(createList);
+
+// Event Listeners  ///////////////////////////////////////////
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (todoInput?.value == '' || todoInput.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: todoInput.value,
+    completed: false,
+    createdAt: new Date()
+  };
+
+  tasks.push(newTask);
+  saveTasks();
+
+  createList(newTask);
+  todoInput.value = '';
+});
+
+// Functions  ///////////////////////////////////////////
+
+function createList(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.setAttribute('id', `${task.id}`);
+  item.append(label);
+  listContainer?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
